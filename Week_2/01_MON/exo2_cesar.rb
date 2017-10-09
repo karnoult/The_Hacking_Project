4f9c18b7f0898b4ca4b4acaa5dee885f1dad85c3
Kévin Arnoult <kevin.arnoult@gmail.com>
2017-10-09 12:31:27 +0200

    Replace int value by the letter so that it's easier to understand

--- Week_2/01_MON/exo2_cesar.rb
+++ Week_2/01_MON/exo2_cesar.rb
@@ -1,17 +1,17 @@
 def chiffre_de_cesar(initial_text, shift)
 	final_text = ""
-	
+
 	#passer sur toutes les lettres du texte initial
 	initial_text.chars.each do |initial_car|
 		
 		#récupérer la valeur ASCII de la lettre
 		initial_car_ascii = initial_car.ord
-		
+
 		#la chiffrer si elle fait partie des lettres suivantes a-zA-Z
-		if (initial_car_ascii >= 65 && initial_car_ascii <= 90) || (initial_car_ascii >= 97 && initial_car_ascii <= 122)
+		if (initial_car_ascii >= "A".ord && initial_car_ascii <= "Z".ord) || (initial_car_ascii >= "a".ord && initial_car_ascii <= "z".ord)
 			
 			#effectuer le décalage, en gérant le débordement possible
-			if (initial_car_ascii + shift > 122) || (initial_car_ascii <= 90 && initial_car_ascii + shift > 90)
+			if (initial_car_ascii + shift > "z".ord) || (initial_car_ascii <= "Z".ord && initial_car_ascii + shift > "Z".ord)
 				final_text << (initial_car_ascii + shift - 26).chr
 			else
 				final_text << (initial_car_ascii + shift).chr
