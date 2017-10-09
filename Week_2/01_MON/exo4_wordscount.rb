e9a31cf3d8c36d6f3039f2f45d889af9dd48df0f
Kévin Arnoult <kevin.arnoult@gmail.com>
2017-10-09 16:32:15 +0200

    Minor improvements and comment

--- Week_2/01_MON/exo4_wordscount.rb
+++ Week_2/01_MON/exo4_wordscount.rb
@@ -2,15 +2,24 @@ def jean_michel_data(corpus, dictionary)
 
 	occurences = {}
 
+	#corpus : update to downcase and split words
+	corpus_words = corpus.downcase.split(' ')
+
+	#navigate through dictionary
 	dictionary.each do |dictionary_word|
-		corpus.downcase.split(' ').each do |corpus_word|
+		
+		#navigate through corpus
+		corpus_words.each do |corpus_word|
+			
 			if corpus_word.include? dictionary_word
+				#add to the occurences hash
 				if occurences.has_key? dictionary_word
 					occurences[dictionary_word] = (occurences[dictionary_word] + 1)
 				else
 					occurences[dictionary_word] = 1
 				end
 			end
+			
 		end
 	end
 
@@ -19,5 +28,6 @@ def jean_michel_data(corpus, dictionary)
 end
 
 corpus = "Howdy partner, sit down! How's it going?"
+
 dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i", "low", "own", "part", "partner", "sit"]	
 jean_michel_data(corpus, dictionary)
