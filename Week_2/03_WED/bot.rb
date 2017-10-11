5587f2a158b2c40204e9e58deaf3a4a62a0521e5
Kévin Arnoult <kevin.arnoult@gmail.com>
2017-10-11 17:59:42 +0200

    Put the spam in a spam function

--- Week_2/03_WED/bot.rb
+++ Week_2/03_WED/bot.rb
@@ -2,47 +2,60 @@
 
 require "twitter"
 
-client_stream = Twitter::Streaming::Client.new do |config|
+$client_stream = Twitter::Streaming::Client.new do |config|
   config.consumer_key        = "XYTYYk9GZrFQ5w3HMzQOsCmoL"
   config.consumer_secret     = ENV['twitter_consumer_secret']
   config.access_token        = "918055468798566400-rsUOtTB8akwmqSRVES462XASbTwgoFP"
   config.access_token_secret = ENV['twitter_access_token_secret']
 end
 
-client_rest = Twitter::REST::Client.new do |config|
+$client_rest = Twitter::REST::Client.new do |config|
   config.consumer_key        = "XYTYYk9GZrFQ5w3HMzQOsCmoL"
   config.consumer_secret     = ENV['twitter_consumer_secret']
   config.access_token        = "918055468798566400-rsUOtTB8akwmqSRVES462XASbTwgoFP"
   config.access_token_secret = ENV['twitter_access_token_secret']
 end
 
-
-def search_twitter(term, options, client_rest)
-  client_rest.search(term, options).take(10).each do |tweet|
-  if tweet.is_a?(Twitter::Tweet)
-    puts tweet.text
+# retweet, fav tweet, and follow user
+def spam(tweet)
+  begin
+    $client_rest.retweet!(tweet)
+    #$client_rest.update("hello @#{tweet.user.screen_name}, tu peux manger pas cher avec mon code promo UberEats : xnbhu")
+    $client_rest.favorite!([tweet])
+    $client_rest.follow!([tweet.user])
+  rescue Twitter::Error
+    puts ">>> This tweet has already been treated"
   end
 end
-end
 
-def filter_twitter(options, client_stream, client_rest)
-  client_stream.filter(options) do |tweet|
-    if tweet.is_a?(Twitter::Tweet) && !tweet.text.start_with?("RT ")
-
-      begin
+def search_twitter(term, options, nb_results)
+  begin
+    $client_rest.search(term, options).take(nb_results).each do |tweet|
+      if tweet.is_a?(Twitter::Tweet)
         puts tweet.text
-        client_rest.retweet!(tweet)
-        #client_rest.update("hello @#{tweet.user.screen_name}, tu peux manger pas cher avec mon code promo UberEats : xnbhu")
-        client_rest.favorite([tweet])
-        client_rest.follow([tweet.user])
-      rescue Twitter::Error::Unauthorized
-        puts "This one has already been treated"
+        #puts $client_rest.rate_limit.to_s
+        spam(tweet)
       end
-
     end
+  rescue Twitter::Error >> e
+    puts ">>> This tweet has already been treated: " + e.to_s
   end
 end
 
+def filter_twitter(options)
+  begin
+    $client_stream.filter(options) do |tweet|
+      if tweet.is_a?(Twitter::Tweet) && !tweet.text.start_with?("RT ")
+        puts tweet.text
+        spam(tweet)
+      end
+    end
+  rescue Twitter::Error => e
+    puts ">>> Exception Class: #{ e.class.name }"
+    puts ">>> Exception Message: #{ e.message }"
+    puts ">>> Exception Backtrace: #{ e.backtrace }"
+  end
+end
 
 search_options = {
   result_type: "recent",
@@ -50,10 +63,10 @@ search_options = {
 }
 
 filter_options = {
-  track: "faim",
   result_type: "recent",
-  locations: "48.8566667,2.3509871,10mi"
+  locations: "48.8566667,2.3509871,10mi",
+  track: "faim"
 }
 
-search_twitter("faim", search_options, client_rest)
-#filter_twitter(filter_options, client_stream, client_rest)
+#search_twitter("faim", search_options, 100)
+filter_twitter(filter_options)
