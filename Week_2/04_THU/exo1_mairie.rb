3d490cc29624bb54f8455da938c22a1b9b5cdd52
Kévin Arnoult <kevin.arnoult@gmail.com>
2017-10-12 16:31:29 +0200

    Optimize solution

--- Week_2/04_THU/exo1_mairie.rb
+++ Week_2/04_THU/exo1_mairie.rb
@@ -5,7 +5,7 @@ require 'open-uri'
 require 'rest-client'
 
 # return the townhall email adress from its annuaire-des-mairies.com url
-def get_info_townhall_from_webpage(url_townhall)
+def get_email_townhall_from_webpage(url_townhall)
   email_townhall = ""
   page_townhall = Nokogiri::HTML(open(url_townhall))
 
@@ -18,35 +18,30 @@ def get_info_townhall_from_webpage(url_townhall)
     end
   end
 
-  # get city name
-  h1_content = page_townhall.xpath("//h1")[0].text
-  name_townhall = h1_content["Commune de ".size, h1_content.size]
-
-  return { :name => name_townhall, :email=> email_townhall }
+  return email_townhall
 end
 
 # return all city URLs of a department page from annuaire-des-mairies.com
 def get_townhalls_urls(url_department)
 
-  list_townhall_urls = []
+  list_info_townhall = []
 
   page_department = Nokogiri::HTML(open(url_department))
 
   page_department.xpath("//a[@class=\"lientxt\"]").each do |node|
-    list_townhall_urls << "http://annuaire-des-mairies.com/" + node[:href][1, node[:href].size - 1]
-    #list_urls << { city: node.text, url:  node[:href]}
+    list_info_townhall << { name: node.text, url:  "http://annuaire-des-mairies.com/" + node[:href][1, node[:href].size - 1]}
   end
 
-  return list_townhall_urls
+  return list_info_townhall
 end
 
 url_department = "http://annuaire-des-mairies.com/val-d-oise.html"
 townhall_list = []
 
-get_townhalls_urls(url_department).each do |url_townhall|
-  info_townhall = get_info_townhall_from_webpage(url_townhall)
-  if info_townhall[:email] != ""
-    townhall_list << info_townhall
+get_townhalls_urls(url_department).each do |info_townhall|
+  email_townhall = get_email_townhall_from_webpage(info_townhall[:url])
+  if email_townhall != ""
+    townhall_list << { :name => info_townhall[:name], :email=> email_townhall }
   end
 end
 
