dd6406f1d77cee60aedbe58bb74f49b7a80c3aa1
Kévin Arnoult <kevin.arnoult@gmail.com>
2017-10-12 17:29:13 +0200

    Resolve exo2 w2d4

---
+++ Week_2/04_THU/exo2_trader.rb
@@ -0,0 +1,39 @@
+require 'rubygems'
+require 'nokogiri'
+require 'pry'
+require 'open-uri'
+require 'rest-client'
+
+def get_crypto_and_price
+
+  page_coinmarketcap = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
+
+  list_crypto_and_price = []
+
+  page_coinmarketcap.xpath("//a[@class=\"price\"]").each do |node|
+    list_crypto_and_price << {
+      name: node[:href]["/currencies/".size, node[:href].size].split("\/")[0],
+      price: node.text
+    }
+  end
+
+  return list_crypto_and_price
+
+end
+
+i = 1
+
+loop {
+  get_crypto_and_price.each do |crypto_and_price|
+    puts i.to_s + " > " + crypto_and_price[:name].to_s + " (" + crypto_and_price[:price].to_s + ")"
+  end
+
+  # let's sleep for 10 seconds (would be 3600 for an hour)
+  sleep(10)
+
+  # let's exit after 5 times (would be 24 for a day if using 3600 above)
+  exit if i == 5
+
+  i += 1
+
+}
