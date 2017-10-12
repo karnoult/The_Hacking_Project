81b4e319a0cd3099604fe773026a208810561bfd
Kévin Arnoult <kevin.arnoult@gmail.com>
2017-10-12 09:53:19 +0200

    Clean and use class

--- Week_2/03_WED/TwitterBot.rb
+++ Week_2/03_WED/TwitterBot.rb
@@ -16,45 +16,49 @@ $client_rest = Twitter::REST::Client.new do |config|
   config.access_token_secret = ENV['twitter_access_token_secret']
 end
 
-# retweet, fav tweet, and follow user
-def spam(tweet)
-  begin
-    $client_rest.retweet!(tweet)
-    #$client_rest.update("hello @#{tweet.user.screen_name}, tu peux manger pas cher avec mon code promo UberEats : xnbhu")
-    $client_rest.favorite!([tweet])
-    $client_rest.follow!([tweet.user])
-  rescue Twitter::Error
-    puts ">>> This tweet has already been treated"
-  end
-end
+class TwitterBot
 
-def search_twitter(term, options, nb_results)
-  begin
-    $client_rest.search(term, options).take(nb_results).each do |tweet|
-      if tweet.is_a?(Twitter::Tweet)
-        puts tweet.text
-        #puts $client_rest.rate_limit.to_s
-        spam(tweet)
-      end
+  # retweet, fav tweet, and follow user
+  def spam(tweet)
+    begin
+      $client_rest.retweet!(tweet)
+      #$client_rest.update("hello @#{tweet.user.screen_name}, tu peux manger pas cher avec mon code promo UberEats : xnbhu")
+      $client_rest.favorite!([tweet])
+      $client_rest.follow!([tweet.user])
+    rescue Twitter::Error
+      puts ">>> This tweet has already been treated"
     end
-  rescue Twitter::Error >> e
-    puts ">>> This tweet has already been treated: " + e.to_s
   end
-end
 
-def filter_twitter(options)
-  begin
-    $client_stream.filter(options) do |tweet|
-      if tweet.is_a?(Twitter::Tweet) && !tweet.text.start_with?("RT ")
-        puts tweet.text
-        spam(tweet)
+  def search_twitter(term, options, nb_results)
+    begin
+      $client_rest.search(term, options).take(nb_results).each do |tweet|
+        if tweet.is_a?(Twitter::Tweet)
+          puts tweet.text
+          #puts $client_rest.rate_limit.to_s
+          spam(tweet)
+        end
       end
+    rescue Twitter::Error >> e
+      puts ">>> This tweet has already been treated: " + e.to_s
     end
-  rescue Twitter::Error => e
-    puts ">>> Exception Class: #{ e.class.name }"
-    puts ">>> Exception Message: #{ e.message }"
-    puts ">>> Exception Backtrace: #{ e.backtrace }"
   end
+
+  def filter_twitter(options)
+    begin
+      $client_stream.filter(options) do |tweet|
+        if tweet.is_a?(Twitter::Tweet) && !tweet.text.start_with?("RT ")
+          puts tweet.text
+          spam(tweet)
+        end
+      end
+    rescue Twitter::Error => e
+      puts ">>> Exception Class: #{ e.class.name }"
+      puts ">>> Exception Message: #{ e.message }"
+      puts ">>> Exception Backtrace: #{ e.backtrace }"
+    end
+  end
+
 end
 
 search_options = {
@@ -68,5 +72,6 @@ filter_options = {
   track: "faim"
 }
 
-#search_twitter("faim", search_options, 100)
-filter_twitter(filter_options)
+my_bot = TwitterBot.new
+my_bot.search_twitter("faim", search_options, 100)
+#my_bot.filter_twitter(filter_options)
