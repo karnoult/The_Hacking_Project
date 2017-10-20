c4667cfe5511bd444b0aedcace01714567053edc
Kévin Arnoult <kevin.arnoult@gmail.com>
2017-10-20 18:22:44 +0200

    Improve my web guesser

--- Week_3/05_FRI/web_guesser.rb
+++ Week_3/05_FRI/web_guesser.rb
@@ -3,28 +3,36 @@ require 'sinatra/reloader'
 
 class WebGuesser
 
-  attr_accessor :value_to_guess
+  :max_below
+  :min_above
 
   def initialize
     reset
   end
 
   def check_guess(user_value)
-    if user_value > value_to_guess
-      @values_entered << "<mark class='too_high'>#{user_value}</mark>"
-      return ["Your last guess is too high", @values_entered]
-    elsif user_value < value_to_guess
-      @values_entered << "<mark class='too_low'>#{user_value}</mark>"
-      return ["Your last guess is too low", @values_entered]
+    msg = ""
+    if user_value > @value_to_guess
+      @min_above = user_value if user_value < @min_above
+      return ["Your last guess (#{user_value}) is too high", get_helper]
+    elsif user_value < @value_to_guess
+      @max_below = user_value if user_value > @max_below
+      return ["Your last guess (#{user_value}) is too low", get_helper]
     else
-      @values_entered << "<mark class='right_guess'>#{user_value}</mark>"
-      return ["Your last guess is the right guess!", @values_entered]
+      return ["<strong>Your last guess (#{user_value}) is the right guess!</strong>", ""]
     end
+
+
+  end
+
+  def get_helper
+    return "<mark class='too_low'>#{@max_below}</mark>  >>>  ?  <<<  <mark class='too_high'>#{@min_above}</mark>"
   end
 
   def reset
     @value_to_guess = rand(100)
-    @values_entered = []
+    @max_below = 0
+    @min_above = 100
   end
 end
 
@@ -33,7 +41,7 @@ the_web_guesser = WebGuesser.new
 get '/' do
   if params["guess"].nil?
     the_web_guesser.reset
-    erb :index, :locals => {:result => ["It's time for you to enter a value!", []]}
+    erb :index, :locals => {:result => ["It's time for you to enter a value!", ""]}
   else
     erb :index, :locals => {:result => the_web_guesser.check_guess(params["guess"].to_i)}
   end
