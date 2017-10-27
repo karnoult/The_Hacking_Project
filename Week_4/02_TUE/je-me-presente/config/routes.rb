4f8230d852f3be2d7b3c01c5f7b50f282cee720d
Kévin Arnoult <kevin.arnoult@gmail.com>
2017-10-27 14:36:07 +0200

    do dynamic pages

--- Week_4/02_TUE/je-me-presente/config/routes.rb
+++ Week_4/02_TUE/je-me-presente/config/routes.rb
@@ -2,11 +2,6 @@ Rails.application.routes.draw do
 
   root to: 'static_pages#home', as: 'home'
 
-  get 'contact', to: 'static_pages#contact', as: 'contact'
-
   get 'about(/:id)', to: 'static_pages#about', as: 'about'
-
-  get 'toi', to: 'static_pages#toi', as: 'toi'
-  get 'binome', to: 'static_pages#binome', as: 'binome'
 
 end
