82c4e898dfe3e31dda914cd32ade1fa0721d6e2c
Kévin Arnoult <kevin.arnoult@gmail.com>
2017-11-03 15:14:16 +0100

    Seeds

--- Week_5/05_FRI/THP_EventBrite/db/seeds.rb
+++ Week_5/05_FRI/THP_EventBrite/db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ua = User.create(name: "Alex")
+ub = User.create(name: "Brice")
+ud = User.create(name: "Duy")
+uj = User.create(name: "Juliette")
+uk = User.create(name: "Kévain")
+un = User.create(name: "Naula")
+
+Event.create(desc: "Ruby", place: "Paris", date: "2017-11-15", creator: ua)
+Event.create(desc: "Rails", place: "Tours", date: "2017-11-12", creator: ub)
+Event.create(desc: "Cuisine éthiopienne", place: "Tours", date: "2017-11-15", creator: ua)
+Event.create(desc: "Tennis pour les nuls", place: "Vincennes", date: "2017-11-15", creator: uk)
+Event.create(desc: "Hack ton tennis#", place: "Paris", date: "2017-11-15", creator: uk)
+Event.create(desc: "jQuery 101", place: "Paris", date: "2017-11-15", creator: uk)
