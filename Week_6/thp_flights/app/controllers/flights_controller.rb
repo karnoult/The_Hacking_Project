eb9ba606b345132e5b3276be9a4a5cd791ab67af
Kévin Arnoult <kevin.arnoult@gmail.com>
2017-11-06 17:48:21 +0100

    Update the index view table

--- Week_6/thp_flights/app/controllers/flights_controller.rb
+++ Week_6/thp_flights/app/controllers/flights_controller.rb
@@ -1,6 +1,10 @@
 class FlightsController < ApplicationController
   def index
+    @airports = Airport.all
+
     if params_flights && params_flights[:departure_airport_id] && params_flights[:arrival_airport_id]
+      @departure_airport_code = Airport.find(params_flights[:departure_airport_id]).airport_code
+      @arrival_airport_code = Airport.find(params_flights[:arrival_airport_id]).airport_code
       @flights = Flight.search(params_flights)
     end
   end
