7747e63283f2ba4bb51bcdd2bc8e0299cb3bb3b3
Kévin Arnoult <kevin.arnoult@gmail.com>
2017-11-06 17:24:25 +0100

    Only show flights if a search has been done

--- Week_6/thp_flights/app/controllers/flights_controller.rb
+++ Week_6/thp_flights/app/controllers/flights_controller.rb
@@ -1,10 +1,7 @@
 class FlightsController < ApplicationController
   def index
-    @flights = []
     if params_flights && params_flights[:departure_airport_id] && params_flights[:arrival_airport_id]
       @flights = Flight.search(params_flights)
-    else
-      @flights = Flight.first(5)
     end
   end
 
