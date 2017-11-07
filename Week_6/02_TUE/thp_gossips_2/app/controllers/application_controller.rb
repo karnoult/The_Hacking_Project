94b74b8c1ab263452096982f9269fe6b847b9059
Kévin Arnoult <kevin.arnoult@gmail.com>
2017-11-07 12:07:24 +0100

    Add header and fix typo in ApplicationController

--- Week_6/02_TUE/thp_gossips_2/app/controllers/application_controller.rb
+++ Week_6/02_TUE/thp_gossips_2/app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :sanitize_devise_params, if :devise_controller?
+  before_action :sanitize_devise_params, if: :devise_controller?
 
   protected
 
