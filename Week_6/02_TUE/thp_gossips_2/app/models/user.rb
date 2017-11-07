4f0fbb5095692286baa75e07e59c624f97f38690
Kévin Arnoult <kevin.arnoult@gmail.com>
2017-11-07 19:22:38 +0100

    Add other validations to User

--- Week_6/02_TUE/thp_gossips_2/app/models/user.rb
+++ Week_6/02_TUE/thp_gossips_2/app/models/user.rb
@@ -7,7 +7,11 @@ class User < ApplicationRecord
 
   has_many :gossips, foreign_key: 'creator_id'
 
-  validates :username, presence: true, uniqueness: true, length: { in: 3..10 }
+  validates :username,
+    presence: true,
+    uniqueness: { case_insensitive: true },
+    length: { in: 3..10 },
+    format: { with: /\A[a-zA-Z0-9_]*\z/ }
 
   attr_accessor :sign_up_code
   validates :sign_up_code,
