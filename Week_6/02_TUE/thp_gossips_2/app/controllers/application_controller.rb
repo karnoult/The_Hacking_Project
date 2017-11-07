d9b9073cdc6acb34af894dc4ba7c4c9d8d17e5bd
Kévin Arnoult <kevin.arnoult@gmail.com>
2017-11-07 16:08:17 +0100

    Allow editing username; allow editing and deleting of own gossips

--- Week_6/02_TUE/thp_gossips_2/app/controllers/application_controller.rb
+++ Week_6/02_TUE/thp_gossips_2/app/controllers/application_controller.rb
@@ -7,5 +7,6 @@ class ApplicationController < ActionController::Base
 
   def sanitize_devise_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
 end
