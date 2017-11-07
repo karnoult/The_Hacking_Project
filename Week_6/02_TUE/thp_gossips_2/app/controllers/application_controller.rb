e493c2203fa079ceda602f1fb2333cc06a6f3d87
Kévin Arnoult <kevin.arnoult@gmail.com>
2017-11-07 11:29:17 +0100

    Add a username to user

--- Week_6/02_TUE/thp_gossips_2/app/controllers/application_controller.rb
+++ Week_6/02_TUE/thp_gossips_2/app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :sanitize_devise_params, if :devise_controller?
+
+  protected
+
+  def sanitize_devise_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
 end
