443d92ed959067c9d6df64c96b14f96a204effdb
Kévin Arnoult <kevin.arnoult@gmail.com>
2017-11-07 14:25:59 +0100

    Add Gossip actions and index view

--- Week_6/02_TUE/thp_gossips_2/app/controllers/gossips_controller.rb
+++ Week_6/02_TUE/thp_gossips_2/app/controllers/gossips_controller.rb
@@ -1,2 +1,42 @@
 class GossipsController < ApplicationController
+
+  def index
+    @gossips = Gossip.all
+  end
+
+  def new
+    @gossip = Gossip.new
+  end
+
+  def create
+    @gossip = Gossip.new
+    @gossip.content = params_gossip[:content]
+    @gossip.creator_id = User.find(params_gossip[:creator_id])
+    @gossip.creation_datetime = params_gossip[:creation_datetime]
+
+    if @gossip.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @gossip = Gossip.find(params[:id])
+  end
+
+  def update
+    @gossip = Gossip.find(params[:id])
+  end
+
+  def destroy
+    @gossip = Gossip.find(params[:id])
+  end
+
+  private
+
+  def params_gossip
+    params.require(:gossip).permit(:content, :creator_id, :creation_datetime)
+  end
+
 end
--- Week_6/02_TUE/thp_gossips_2/config/routes.rb
+++ Week_6/02_TUE/thp_gossips_2/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'gossips#index'
   devise_for :users
+  resources 'gossips'
 end
