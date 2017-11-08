35fa4ff5fec5f5c66fb7abf6ad94c40606812364
Kévin Arnoult <kevin.arnoult@gmail.com>
2017-11-08 16:07:09 +0100

    Enable JS

---
+++ Week_6/03_WED/pipeline/app/assets/javascripts/main.js
@@ -0,0 +1,20 @@
+function initJS() {
+	let centre = document.querySelector('section');
+	centre.style.display = "none";
+	let boutonCentral = document.querySelector("#central");
+	boutonCentral.onclick= function (){
+		centre.style.display = "flex";
+		document.querySelector('#section_bouton').style.display= "none";
+
+	}
+	document.querySelector("#rech").onclick = function(){
+		if (document.querySelector("#search").value !== "")
+			document.location.href="https://www.google.com/#q=" + document.querySelector("#search").value.split(" ").join("+");
+
+	}
+	function alertX() {
+		alert ("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please!");
+		// body...
+	}
+	window.setTimeout("alertX()",10000);
+}
