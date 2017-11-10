080bc2d6ef046c8d9272bb1daddf9a1ff4fd08f9
Kévin Arnoult <kevin.arnoult@gmail.com>
2017-11-10 11:33:22 +0100

    Remove notes from git repo

--- Docs/Rails/Relations/event.rb
+++
@@ -1,4 +0,0 @@
-class Event < ApplicationRecord
-  belongs_to :creator, class_name: "User"
-  has_and_belongs_to_many :attendees, class_name: "User"
-end
--- Docs/Rails/params_controllers.rb
+++
@@ -1,9 +0,0 @@
-class EventsController < ApplicationController
-  ...
-
-  private
-
-  def params_event
-    params.require(:event).permit(:desc, :place, :date, :creator_id)
-  end
-end
