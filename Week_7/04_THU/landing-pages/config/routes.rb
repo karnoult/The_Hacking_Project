a0ce2fb28831ada990e678d423f8f4316b103ccc
Kévin Arnoult <kevin.arnoult@gmail.com>
2017-11-16 16:46:24 +0100

    Set checkr as HP

--- Week_7/04_THU/landing-pages/config/routes.rb
+++ Week_7/04_THU/landing-pages/config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get 'home/checkr'
-
-  get 'home/sparks'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#checkr'
 end
