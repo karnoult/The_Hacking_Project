6420218150d9174d977a998e55ef496c086955bd
Kévin Arnoult <kevin.arnoult@gmail.com>
2017-11-23 17:22:44 +0100

    Deliver project w8d4

--- Week_8/04_THU/testing_jquery/app/assets/javascripts/index.js
+++ Week_8/04_THU/testing_jquery/app/assets/javascripts/index.js
@@ -1,1 +1,9 @@
-console.log($(".screen"));
+var screen_shown = 1;
+
+$(':button').click(function() {
+    if(screen_shown != this.value) {
+        $('#screen_' + screen_shown).slideUp();
+        screen_shown = this.value;
+        $('#screen_' + screen_shown).slideDown();
+    }
+});
