eaf91e1c88f733dc936e33b45a994a0fa2fb0919
Kévin Arnoult <kevin.arnoult@gmail.com>
2017-11-28 15:08:29 +0100

    Show errors

--- Week_9/01_MON/api_maps/app/controllers/home_controller.rb
+++ Week_9/01_MON/api_maps/app/controllers/home_controller.rb
@@ -3,14 +3,22 @@ require 'pry'
 class HomeController < ApplicationController
   def index
     @map = Map.new
-    @map.latitude = 45 # default value
-    @map.longitude = 0 # default value
-    @map.message = 'default marker' # default value
     # binding.pry
     return unless param_maps[:map] && param_maps[:map][:latitude] && param_maps[:map][:longitude] && param_maps[:map][:message]
     @map.latitude = param_maps[:map][:latitude]
     @map.longitude = param_maps[:map][:longitude]
     @map.message = param_maps[:map][:message]
+    unless @map.valid?
+      @map.errors.full_messages.each do |error|
+        flash.now[:danger] = error
+      end
+    end
+  end
+
+  def default_map
+    @map.latitude = 45 # default value
+    @map.longitude = 0 # default value
+    @map.message = 'default marker' # default value
   end
 
   def param_maps
--- Week_9/01_MON/api_maps/app/models/map.rb
+++ Week_9/01_MON/api_maps/app/models/map.rb
@@ -3,7 +3,13 @@ class Map < ApplicationRecord
   attr_accessor :longitude
   attr_accessor :message
 
-  validates :latitude, presence: true
-  validates :longitude, presence: true
-  validates :message, presence: true, length: { maximum: 10 }
+  validates :latitude, presence: true, numericality: {
+    greater_than_or_equal_to: -90,
+    less_than_or_equal_to: 90
+  }
+  validates :longitude, presence: true, numericality: {
+    greater_than_or_equal_to: -180,
+    less_than_or_equal_to: 180
+  }
+  validates :message, presence: true, length: { maximum: 50 }
 end
