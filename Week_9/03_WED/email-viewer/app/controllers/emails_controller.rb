d513ad436be0aa20adf702946d196940d34f883a
Kévin Arnoult <kevin.arnoult@gmail.com>
2017-11-29 14:56:59 +0100

    Display email content

--- Week_9/03_WED/email-viewer/app/controllers/emails_controller.rb
+++ Week_9/03_WED/email-viewer/app/controllers/emails_controller.rb
@@ -2,4 +2,12 @@ class EmailsController < ApplicationController
   def index
     @emails = Email.all
   end
+
+  def show_content
+    @email = Email.find(params[:id])
+
+    respond_to do |format|
+      format.js
+    end
+  end
 end
--- Week_9/03_WED/email-viewer/config/routes.rb
+++ Week_9/03_WED/email-viewer/config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'emails#index'
+  get 'show_content', to: 'emails#show_content'
 end
